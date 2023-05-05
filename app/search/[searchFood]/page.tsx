caf6e90bc68426fac7293c48bb72b57fc9e06b2b
Ikar <ikarpiatkowski@proton.me>
2023-05-05 03:58:01 +0200

    Fix params for search

--- app/search/[searchFood]/page.tsx
+++ app/search/[searchFood]/page.tsx
@@ -1,14 +1,16 @@
 import { Suspense } from 'react';
 import SearchNinja from '../SearchNinja';
 
-export default function SearchFood() {
+export default function SearchFood({
+  params: { searchFood },
+}: SearchPageProps) {
   return (
     <>
       <main className="flex flex-col ml-32 items-center justify-center h-screen">
         Food
         <Suspense fallback={<p className="text-red-500">Loading food...</p>}>
           {/* @ts-ignore */}
-          <SearchNinja params={{ searchFood: 'pizza' }} />
+          <SearchNinja params={{ searchFood }} />
         </Suspense>
       </main>
       {/* <main className="flex flex-col items-center justify-center w-full h-screen">
