e1426791f7d66a514d2ce5604917e2874622e98c
Ikar <ikarpiatkowski@proton.me>
2023-07-16 07:03:41 +0200

    Calculate daily nutritional values

--- components/UserFood.tsx
+++ components/UserFood.tsx
@@ -3,6 +3,11 @@ import { useAuth } from '@clerk/nextjs';
 import supabase from '@utils/supabase';
 import { useState } from 'react';
 export default async function UserFood() {
+  let totalCalories = 0;
+  let totalFat = 0;
+  let totalProtein = 0;
+  let totalCarbohydrates = 0;
+  let totalFiber = 0;
   const [datePicker, setDatePicker] = useState('2023-06-21');
   const { userId } = useAuth();
   const { data } = await supabase
@@ -17,8 +22,58 @@ export default async function UserFood() {
         type="date"
         onChange={(e) => setDatePicker(e.target.value)}
       />
+      {/* {data!.reduce((f) => f + f, 0)} */}
+      {data!.forEach((f) => {
+        totalCalories += f.food[0].calories;
+        totalFat += f.food[0].fat_total_g;
+        totalProtein += f.food[0].protein_g;
+        totalCarbohydrates += f.food[0].carbohydrates_total_g;
+        totalFiber += f.food[0].fiber_g;
+      })}
+      <div className="flex">
+        <div className="flex flex-col">
+          {`Calories (${parseFloat(totalCalories.toFixed(2))} / 2000)`}
+          <progress
+            className="w-40 bg-gray-200 rounded-full h-4 dark:bg-gray-700 m-4"
+            value={(totalCalories / 2000) * 100}
+            max="100"
+          ></progress>
+        </div>
+        <div className="flex flex-col">
+          {`Fat (${parseFloat(totalFat.toFixed(2))} / 80)`}
+          <progress
+            className="w-40 bg-gray-200 rounded-full h-4 dark:bg-gray-700 m-4"
+            value={(totalFat / 80) * 100}
+            max="100"
+          ></progress>
+        </div>
+        <div className="flex flex-col">
+          {`Protein (${parseFloat(totalProtein.toFixed(2))} / 100)`}
+          <progress
+            className="w-40 bg-gray-200 rounded-full h-4 dark:bg-gray-700 m-4"
+            value={(totalProtein / 100) * 100}
+            max="100"
+          ></progress>
+        </div>
+        <div className="flex flex-col">
+          {`Carbs (${parseFloat(totalCarbohydrates.toFixed(2))} / 180)`}
+          <progress
+            className="w-40 bg-gray-200 rounded-full h-4 dark:bg-gray-700 m-4"
+            value={(totalCarbohydrates / 180) * 100}
+            max="100"
+          ></progress>
+        </div>
+        <div className="flex flex-col">
+          {`Fiber (${parseFloat(totalFiber.toFixed(2))} / 20)`}
+          <progress
+            className="w-40 bg-gray-200 rounded-full h-4 dark:bg-gray-700 m-4"
+            value={(totalFiber / 20) * 100}
+            max="100"
+          ></progress>
+        </div>
+      </div>
       <div className="flex flex-wrap">
-        {data!.map((f: any) => (
+        {data!.map((f) => (
           <div
             key={f.food[0].name}
             className="rounded-3xl bg-neutral-300 dark:bg-neutral-600 m-4 p-2 w-64"
